d50322b82a6844bef7fd9d7d61cf0fb3289efec4
AnnaSil <108275504+AnnaSil@users.noreply.github.com>
2023-02-06 22:02:52 +0200

    hw-done

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGallery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGallery(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `
+  }).join('');
+}
+
+gallery.addEventListener('click', onItemClick)
+
+let modalWindow
+
+function onItemClick(event) {
+    event.preventDefault();
+
+    if(event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const modalWindow = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+        onShow: modalWindow => {
+            window.addEventListener('keydown', onEscClick)
+        },
+        onClose: modalWindow => {
+            window.addEventListener('keydown', onEscClick)
+        }
+    }
+    )
+
+    function onEscClick(e) {
+        if(e.code === "Escape" && basicLightbox.visible()) {
+            modalWindow.close()
+        }
+    }
+
+    modalWindow.show()
+}
